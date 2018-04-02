862da6ffb8fb2998e1437f204b76de2af6a0374f
Stefano Gioia <stefanogch@gmail.com>
2018-04-02 10:03:57 -0400

    esqueleto modal

--- App.js
+++ App.js
@@ -45,6 +45,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
--- components/Survey.js
+++ components/Survey.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Modal, TouchableHighlight, Text, View, StyleSheet } from 'react-native';
 
 
 export default class Survey extends React.Component {
+  state = {
+    modalVisible: false,
+  };
+
+  toggleModalVisibility() {
+    this.setState({ modalVisible: !this.state.modalVisible });
+  }
+
   render() {
     return( 
-      <View>
-        <Text>Survey</Text>
+      <View style={{ flexDirection: 'column', flex: 1}}>
+        <View style={{ flex: 0.5 }}/>
+        <View style={{ flexDirection: 'row', flex: 1}}>
+          <View style={{ flex: 0.1}}/>
+          <View style={ styles.modalContainer }>
+            <Text>Hola</Text>
+          </View>
+        </View>
+        <View style={{ flex: 0.5 }}/>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 0.8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  }
+});
