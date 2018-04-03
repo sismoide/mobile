e69dea6f1c60c1e9fd1477d301ff914853243c06
Stefano Gioia <stefanogch@gmail.com>
2018-04-03 09:45:13 -0400

    Navigation (#24)
    * dependencia react-navigation
    
    * navegación
    
    * esqueleto modal
    
    * hacer un componente para el modal
    
    * modal sabe cómo organizarse
    
    * modal header
    
    * abstraer un poco opciones
    
    * Added button (#23)
    
    * Added button
    
    * Quake Button rdy to use
    
    * rm add.js
    
    * agregar el botón'

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Survey from './components/Survey.js';
+import QuakeButton from './components/QuakeButton.js';
 
-export default class App extends React.Component {
+import BaseStyle from './styles/base.js';
+
+class Home extends React.Component {
+  static navigationOptions = {
+    title: 'Sismoide',
+    headerStyle: {
+      backgroundColor: BaseStyle.colors.secondaryBackgroundColor
+    },
+    headerTintColor: BaseStyle.colors.secondaryTextColor,
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <QuakeButton/>
       </View>
     );
   }
 }
+
+export default StackNavigator(
+  {
+    Home: {
+      screen: Home
+    },
+    Survey: {
+      screen: Survey 
+    },
+  },
+  {
+    initialRouteName: 'Home'
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
