9154b611738b36d0b2c4cd9ecca97ef61447590f
Stefano Gioia <stefanogch@gmail.com>
2018-04-16 12:33:57 -0300

    módulo para guardar reportes

---
+++ database/storage.js
@@ -0,0 +1,53 @@
+import { AsyncStorage } from 'react-native';
+
+const QUAKE_REPORTS_KEY = '@QuakeReports:all';
+
+export default class Storage {
+  /**
+   * Store a quake report
+   *
+   * @param { Object } - geolocation: Where the quake submission is coming from.
+   * @returns { Promise }
+   */
+  async submitQuakeReport(geolocation) {
+    let existingReports = null;
+    try {
+      // Check whether there are previous quake submissions.
+      existingReports = await AsyncStorage.getItem(QUAKE_REPORTS_KEY);
+      existingReports = JSON.parse(existingReports);
+    } catch (error) { }
+    const createReport = function(geolocation) {
+      return {
+        timestamp: `${ Date.now() }`,
+        geolocation: geolocation
+      }
+    };
+    if (existingReports) {
+      // If there were previous quake submissions, push to them.
+      existingReports.push(createReport(geolocation));
+      return AsyncStorage.setItem(
+        QUAKE_REPORTS_KEY,
+        JSON.stringify(existingReports));
+    } else {
+      // Else, we need to create the key-value pair
+      return AsyncStorage.setItem(
+        QUAKE_REPORTS_KEY,
+        JSON.stringify([ createReport(geolocation) ]));
+    }
+  }
+
+  /**
+   * @returns { Promise }: All recorded quake reports
+   */
+  async getQuakeReports() {
+    try {
+      return JSON.parse(await AsyncStorage.getItem(QUAKE_REPORTS_KEY));
+    } catch(error) {
+      return [];
+    }
+  }
+
+  submitSurvey(intensity) {
+
+  }
+}
